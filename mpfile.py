d864341b23daf4a71e2a10c7b7a56ea2852eea54
adnan <ahabib@gmal.com>
2019-07-03 17:02:39 -0700

    fixed execution. This is now in a good state.

--- mpfile.py
+++ mpfile.py
@@ -28,43 +28,62 @@ def discover_path_gen(root_path, file_chunk_size):
 
 
 def read_chunk(filename, start_chunk):
-    logging.debug("Reading %s at %s", filename, start_chunk)
+    logging.debug("\t\tReading file segment %s at %s", os.path.basename(filename), start_chunk)
     with open(filename, "rb") as file:
         file.seek(start_chunk)
         data = file.read(CHUNK_SIZE)
         if logging.getLogger().level == logging.DEBUG:
             hash_segment = hashlib.sha256()
             hash_segment.update(data)
-            logging.debug("\t\t%s %s %s", hash_segment.hexdigest(), start_chunk, filename)
+            logging.debug("\t\t%s %s %s", hash_segment.hexdigest(), start_chunk, os.path.basename(filename))
         return data
+
+
+def stitch_file(data):
+    file_hash = hashlib.sha256()
+    for entry in data:
+        file_hash.update(entry)
+    return file_hash.hexdigest()
 
 
 def process_entry(filename, seek_location):
     try:
         chunk = read_chunk(filename, seek_location)
         segment = files[filename]
-        logging.debug("%s in files entry is %s", filename, segment)
+        logging.debug("\t\t%s in files entry is %s", os.path.basename(filename), segment)
+        logging.debug("\t\tSegments processed (BEFORE): %s", files.get(filename))
+        # with sync_access:
         files[filename] -= 1
-        if files[filename] < 0:
-            logging.debug("file data exist: %s", "YES" if filename in files_data else "False")
-            hash_func = hashlib.sha256()
+        logging.debug("\t\tSegments processed (AFTER): %s", files.get(filename))
+        stitch_data = []
+        if files[filename] < 1:
+            logging.debug("\t\t\tfile data exist: %s", "YES" if filename in files_data else "False")
             if filename in files_data:
                 file_data = files_data[filename]
+                logging.debug("\t\t\tInserting segment %s with length %s", seek_location, len(chunk))
+                file_data.append([seek_location, chunk])
                 segments = len(file_data)
-                if segments > 1:
-                    logging.debug("UNSORTED (%s): %s", segments, [cd[0] for cd in file_data])
+                if segments > 0:
+                    logging.debug("\t\t\t\tUNSORTED (%s): %s", segments, [cd[0] for cd in file_data])
                     file_data.sort()
-                    logging.debug("SORTED (%s): %s", segments, [cd[0] for cd in file_data])
+                    logging.debug("\t\t\t\tSORTED (%s): %s", segments, [cd[0] for cd in file_data])
                 for chunk_data in file_data:
-                    hash_func.update(chunk_data[1])
-            hash_func.update(chunk)
-            calculated_hash = hash_func.hexdigest()
-            logging.info("%s %s", calculated_hash, filename)
-            # print(calculated_hash, filename)
-            # del files[filename]
-            # del files_data[filename]
+                    logging.debug("\t\t\t\tUpdating data segment %s (%s)", chunk_data[0], len(chunk_data[1]))
+                    stitch_data.append(chunk_data[1])
+            else:
+                logging.debug("\t\t\t\tUpdating data segment %s (%s)", seek_location, len(chunk))
+                stitch_data.append(chunk)
+            logging.debug("\t\t\t\t\tCalculate hash: %s, segments: %s", os.path.basename(filename), len(stitch_data))
+            calculated_hash = stitch_file(stitch_data)
+            # logging.info("%s %s", calculated_hash, filename)
+            with sync_access:
+                print(calculated_hash, filename)
+            del files[filename]
+            del files_data[filename]
         else:
-            files_data.setdefault(filename, []).append([seek_location, chunk])
+            with sync_access:
+                logging.debug("\t\tAdding %s segment at %s (%s)", os.path.basename(filename), seek_location, len(chunk))
+                files_data.setdefault(filename, []).append([seek_location, chunk])
     finally:
         sync_queue.task_done()
 
@@ -74,21 +93,22 @@ def consume_queue(files_to_be_processed_queue, executor):
     futures = []
     while not files_to_be_processed_queue.empty() or not all_files_fed.is_set():
         entry = files_to_be_processed_queue.get()
-        logging.debug("Read queue: %s", entry)
+        logging.debug("Reading queue: %s", entry)
         filename = entry[0]
         seek_location = entry[1]
         futures.append(executor.submit(process_entry, filename, seek_location))
     logging.debug("END Qsize is %s", files_to_be_processed_queue.qsize())
     logging.debug("Waiting for reads to complete...")
     concurrent.futures.wait(futures)
-    logging.debug("Reading complete...")
+    logging.debug("Processed all files...")
 
 
 # may block if queue is full
 def create_queue(path, file_queue):
     for file in discover_path_gen(path, CHUNK_SIZE):
         logging.debug("Enqueueing file: %s", file)
-        files[file[0]] = int(file[1])
+        # with sync_access:
+        files[file[0]] = int(file[1]) + 1
         for chunk in (loc*CHUNK_SIZE for loc in range(file[1]) if file[1] > 0):
             logging.debug("\t\tQueueing some chunk: %s - %s, enqueuing [%s]",
                           chunk, chunk+CHUNK_SIZE-1, file_queue.qsize())
@@ -96,8 +116,8 @@ def create_queue(path, file_queue):
         logging.debug("\t\tQueueing last chunk: %s (%s), enqueuing at %s", file[1]*CHUNK_SIZE,
                       file[2], file_queue.qsize())
         file_queue.put([file[0], file[1] * CHUNK_SIZE, file[1] * CHUNK_SIZE + file[2]])
+    logging.debug("Enqueued all files...")
     all_files_fed.set()
-    logging.debug("Enqueued everything...waiting for all threads to be processed.")
 
 
 def main(files_location, read_queue):
@@ -108,15 +128,17 @@ def main(files_location, read_queue):
 
 if __name__ == '__main__':
     try:
-        logging.basicConfig(format='[%(threadName)s] %(asctime)s %(message)s', level=logging.DEBUG)
+        logging.basicConfig(format='[%(threadName)s] %(asctime)s %(message)s', level=logging.ERROR
+                            )
         CORES = os.cpu_count()
-        QUEUE_SIZE = CORES
-        CHUNK_SIZE = 1024 * 1024 * 2 # 1M
+        QUEUE_SIZE = CORES * 10
+        CHUNK_SIZE = 1024 * 1024 * 10  # 10M
         files = {}
         files_data = {}
         location = sys.argv[1] if len(sys.argv) > 1 and os.path.exists(sys.argv[1]) else None
         all_files_fed = threading.Event()
         sync_queue = queue.Queue(QUEUE_SIZE)
+        sync_access = threading.RLock()
         if location is not None:
             main(location, sync_queue)
         else:
